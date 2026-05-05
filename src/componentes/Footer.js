14b8c595f4a0c94a6f60d7224dfe5ee495195a94
gabriel-correia948 <correia.santos.gabriel@escola.pr.gov.br>
2026-05-05 12:35:26 -0300

    Add files via upload

---
+++ src/componentes/Footer.js
@@ -0,0 +1,20 @@
+function Footer() {
+
+    return (
+        <footer>
+            <div className="menuFooter">
+                <ul>
+                    <li>Sobre o Curso</li>
+                    <li>Disciplina Curriculares</li>
+                    <li>Disciplina Técnicas</li>
+                    <li>Equipe</li>
+                </ul>
+            </div>
+            <div className="credenciais">
+                <h1>Desenvolvimento por: Gabriel Santos - 2026 </h1>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/componentes/Header.js
@@ -0,0 +1,22 @@
+function Header(){
+
+    return (
+        <header>
+        <div className="headerTopo">
+        <h1>Curso Técnico em Desenvolvimento do Sistema</h1>
+        </div>
+        <navi className="headerMenu">
+            <ul>
+                <li>Home</li>
+                <li>Sobre o Curso</li>
+                <li>Disciplina Curriculares</li>
+                <li>Disciplina Técnicas</li>
+                <li>Equipe</li>
+            </ul>
+        </navi>
+        </header>
+
+    )
+}
+
+export default Header;
